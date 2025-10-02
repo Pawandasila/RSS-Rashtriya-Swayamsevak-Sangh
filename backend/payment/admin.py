6225baa717398b0d697b2bfeaee5cb8ce0ea82ef
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-10-02 23:34:00 +0530

    added Payment model to backend

--- backend/payment/admin.py
+++ backend/payment/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Payment
 # Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'amount', 'status', 'timestamp')
+    search_fields = ('name', 'email', 'phone', 'order_id', 'payment_id')
+    list_filter = ('status', 'timestamp')
+    readonly_fields = ('timestamp',)
