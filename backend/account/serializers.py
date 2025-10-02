07466c18d4d7b836038663735b440ba56e42f69e
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-10-02 12:43:11 +0530

    username field added to serializers

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -4,5 +4,5 @@ from .models import User
 class UserJoinSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password', 'name', 'phone', 'dob', 'user_id']
+        fields = ['email', 'password', 'name', 'phone', 'dob', 'user_id', 'username']
         extra_kwargs = {'password': {'write_only': True}}
