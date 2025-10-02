f48931aeb87a0ce551d6a941c611ae828ae3e64a
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-10-02 13:19:58 +0530

    added dashboard app and integrated user info urls

---
+++ backend/dashboard/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from account.models import User
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        exclude = ['password']  # Exclude password from being serialized
---
+++ backend/dashboard/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+from account.models import User
+
+from .serializers import UserSerializer
+
+class DashboardView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
