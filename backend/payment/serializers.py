bd4d3f7982d4db5d23761dc5e78a9bc75fed79d7
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-10-03 00:25:53 +0530

    remove payment_id from exclude attribute from paytmentserializer

--- backend/payment/serializers.py
+++ backend/payment/serializers.py
@@ -4,4 +4,4 @@ from .models import Payment
 class PaymentSerializer(ModelSerializer):
     class Meta:
         model = Payment
-        exclude = ['timestamp', 'payment_id', 'status']  # Exclude fields that are auto-generated or not needed during creation
+        exclude = ['timestamp', 'status']  # Exclude fields that are auto-generated or not needed during creation
