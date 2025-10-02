49151b438a3f4d9ee19bde90af30a10a10893c9d
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-10-02 19:42:27 +0530

    changed payment url to init

--- backend/payment/urls.py
+++ backend/payment/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import PaymentView
 
 urlpatterns = [
-    path('payments/', PaymentView.as_view(), name='payment-list'),
+    path('init/', PaymentView.as_view(), name='payment-list'),
 ]
