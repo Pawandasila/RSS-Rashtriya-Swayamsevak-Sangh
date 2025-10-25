25fd351e16bf0e7c0e4b6e8bd692048758c8013f
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-10-25 23:18:13 +0530

    added urls for the api

--- backend/volunteer/urls.py
+++ backend/volunteer/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import WingListCreateView, WingDetailView
+from .views import WingListCreateView, WingDetailView, LevelListCreateView, LevelDetailView, DesignationListCreateView, DesignationDetailView, VolunteerListCreateView, VolunteerDetailView
 
 urlpatterns = [
     path('wings/', WingListCreateView.as_view(), name='wing-list-create'),
     path('wings/<int:pk>/', WingDetailView.as_view(), name='wing-detail'),
+
+    path('levels/', LevelListCreateView.as_view(), name='level-list-create'),
+    path('levels/<int:pk>/', LevelDetailView.as_view(), name='level-detail'),
+
+    path('designations/', DesignationListCreateView.as_view(), name='designation-list-create'),
+    path('designations/<int:pk>/', DesignationDetailView.as_view(), name='designation-detail'),
+
+    path('volunteers/', VolunteerListCreateView.as_view(), name='volunteer-list-create'),
+    path('volunteers/<int:pk>/', VolunteerDetailView.as_view(), name='volunteer-detail'),
 ]
