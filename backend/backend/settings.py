91d5ff5fc8cb991b752c22c511180fd02cffcf3d
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-11-08 10:37:09 +0530

    updated media and static urls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -183,9 +183,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'api/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_URL = 'media/'
+MEDIA_URL = 'api/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 
