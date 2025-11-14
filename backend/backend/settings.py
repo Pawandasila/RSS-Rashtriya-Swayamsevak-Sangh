f910e9a7e386d04d5b0504c6256b5b8d16a49c56
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-11-14 23:06:32 +0530

    bugs solved

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,9 +58,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,6 +137,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://www.joinrss.org.in",
     "https://joinrss.org.in",
     "https://www.joinrss.org.in",
+    "https://api.joinrss.org.in"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
